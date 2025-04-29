9c2bc196ec13f96bfcf39dd05a4d2829959790b3
dasfaf4464 <richchc@gmail.com>
2025-04-29 23:52:55 +0900

    connect redis

--- web/config.py
+++ web/config.py
@@ -2,4 +2,4 @@ CLOUDFLARE_TUNNEL_COMMAND = ['C:/Data/cloudflared.exe', 'tunnel', '--url', 'http
 
 MARIADB_COMMAND = ['C:/Program Files/MariaDB 11.7/bin/mysqld.exe', '"C:/Program Files/MariaDB 11.7/data/my.ini"']
 
-REDIS_COMMAND = [' ']
+REDIS_COMMAND = ['C:/Program Files/Redis/redis-server.exe', '--port 6379', '--bind 127.0.0.1',]
