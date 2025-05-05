6d25fc63d14d0a86a62947a4b851ff1e9c44306e
dasfaf4464 <richchc@gmail.com>
2025-05-05 21:05:51 +0900

    함수 관리 파일 변경

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,20 +1,25 @@
 from flask import Flask
 from app.views.index import index_bp
-from app.views.register import register_bp, signup_bp
+from app.views.register import register_bp
 from app.views.home import home_bp
 from app.views.history import history_bp
 from app.views.session import session_bp
-from app.util.login import login_bp, makename_bp
+from app.util.login import login_bp, makename_bp, signup_bp, tempkey_bp
 
 def create_app():
     app = Flask(__name__)
+    #페이지 제공 라우트
     app.register_blueprint(index_bp)
     app.register_blueprint(register_bp)
     app.register_blueprint(home_bp)
     app.register_blueprint(history_bp)
     app.register_blueprint(session_bp)
+    #로그인
     app.register_blueprint(login_bp)
+    #회원가입
     app.register_blueprint(makename_bp)
     app.register_blueprint(signup_bp)
+    #회원확인
+    app.register_blueprint(tempkey_bp)
 
     return app
--- web/app/util/login.py
+++ web/app/util/login.py
@@ -1,9 +1,15 @@
+"""
+회원 관리 api
+로그인, 회원가입-이름생성, 임시키-발급, 회원가입
+"""
 from flask import Blueprint, request, jsonify, url_for, make_response
 from app.util.naming import create_user_name
 from app.manager.db_manager import mariadb_user_manager
 
 login_bp = Blueprint('login', __name__)
 makename_bp = Blueprint('make_name', __name__)
+tempkey_bp = Blueprint('check-key', __name__)
+signup_bp = Blueprint('signup', __name__)
 
 @login_bp.route('/login', methods=['POST'])
 def login():
@@ -37,3 +43,31 @@ def login():
 def generate_name():
     generated_name = create_user_name()
     return jsonify({'name': generated_name})
+
+@tempkey_bp.route('/check-key')
+def check_tempkey():
+    user_cookie = {
+        "user_key":""
+    }
+    
+    client_cookie = request.cookies
+    if not client_cookie.get("user_key"):
+        return 
+
+@signup_bp.route('/signup', methods=["POST"])
+def signup():
+    try:
+        data = request.get_json()
+        user_id = data.get('id')
+        password = data.get('pw')
+        email = data.get('email')
+        name = data.get('name')
+        user_key = request.cookies.get("user_key")
+        
+        # TODO: 여기에 DB 저장 로직 넣기 쿠키에 있는 임시키 포함해서
+        # 예: mariadb_user_manager.put_sql("INSERT INTO userinfo (id, pw, email, name) VALUES (%s, %s, %s, %s)", (user_id, password, email, name))
+
+        return jsonify({"success": True, "message": "회원가입 성공!"})
+
+    except Exception as e:
+        return jsonify({"success": False, "message": "회원가입 실패: " + str(e)}), 500
