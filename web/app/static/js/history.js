0ad429adc8adc865246cf08fc063fb1b6ef38c95
leejiyeoniya <jiyeon040518@naver.com>
2025-05-09 20:57:38 +0900

    index, register 모바일 반응형형

--- web/app/static/js/history.js
+++ web/app/static/js/history.js
@@ -1,3 +1,19 @@
+const actions = ["Unmute를 만난 지 ", "열심히 공부한 지", "질문을 생각한 지", "강연자와 소통한 지",
+    "자신감을 얻은 지", "성공을 꿈꾼 지", "성장을 느낀 지", "문제를 해결한 지", "도전을 시작한 지", 
+    "열정적인 사람이 된 지"];
+
+let currentActionIndex = 0;
+
+function rotateActionText() {
+    const actionElement = document.getElementById("action-text");
+    actionElement.textContent = actions[currentActionIndex];
+    currentActionIndex = (currentActionIndex + 1) % actions.length;
+}
+
+rotateActionText();
+
+setInterval(rotateActionText, 10000);
+
 function myQuestion() {
     const button_color = window.getComputedStyle(document.getElementById("my-question")).backgroundColor;
     document.getElementById('content-box').style.backgroundColor = button_color;
